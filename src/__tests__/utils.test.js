b7feb1040134f66509eb1d6a7c5fc0a70fd8b8b7
Josh Dicker <joshdicker@joshs-mac-mini.mynetworksettings.com>
2025-04-04 11:37:30 -0400

    js-tdd-writing-unit-tests

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,10 @@
-// Your tests here
+
+import { pointsForWord } from "../utils";
+
+describe("pointsForWord", () => {
+  it("returns 7 for the word 'test'", () => {
+    const word = "test";
+    const result = pointsForWord(word);
+    expect(result).toBe(7);
+  });
+});
